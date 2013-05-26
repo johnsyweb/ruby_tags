ac516c2c68f8431dbe3ad9807fcd6426a95bb2ba
Pete Johns <paj-github@johnsy.com>
2013-05-26 11:49:08 +1000

    Add a configuration file

--- run_tags.rb
+++ run_tags.rb
@@ -1,46 +1,53 @@
 #!/usr/bin/env ruby
-#-*-ruby-*-
+# encoding: utf-8
+# -*-ruby-*-
 # A script to run ctags on all .rb files in a project. Can be run on
 # the current dir, called from a git callback, or install itself as a
 # git post-merge and post-commit callback.
 
-CTAGS = %x{ which ctags }.chomp
-HOOKS = %w{ post-merge post-commit post-checkout }
-HOOKS_DIR = '.git/hooks'
-VIM_MODE = /_vim/.match($0)
-TAGS_FILE = VIM_MODE ? 'tags' : 'TAGS'
-EMACS_FLAG = VIM_MODE ? '' : '-e'
+require 'yaml'
 
-DEBUG_MODE = false
+config_file = File.expand_path('~/.run_tags.yml')
+config = File.file?(config_file) ? YAML.load_file(config_file) : {}
+DEBUG_MODE = config['debug'] || false
+CTAGS = config['ctags'] || `which ctags`.chomp
+HOOKS = config['hooks'] || %w( post-merge post-commit post-checkout )
+HOOKS_DIR = config['hooks_file'] || '.git/hooks'
+TAGS_FILE = config['tags_file'] || 'TAGS'
+CTAGS_FLAGS = config['ctags_flags'] || '-e'
 
 def install
-  if !File.writable?(HOOKS_DIR)
-    $stderr.print "The install option [-i] can only be used within a git repo; exiting.\n"
+  unless File.writable?(HOOKS_DIR)
+    $stderr.print(
+      "The install option [-i] can only be used within a git repo; exiting.\n")
     exit 1
   end
-  
+
   HOOKS.each { |hook| install_hook("#{HOOKS_DIR}/#{hook}") }
 end
 
 def run_tags(dir, run_in_background = false)
-  if File.executable?(CTAGS) and File.writable?(dir)
-    cmd = "find #{dir} -name \\\*.rb | #{CTAGS} #{EMACS_FLAG} -f #{dir}/#{TAGS_FILE} -L - 2>>/dev/null "
-    cmd << '&' if run_in_background
-    $stderr.print "[#{$0}] calling #{cmd}\n" if DEBUG_MODE
+  if File.executable?(CTAGS) && File.writable?(dir)
+    cmd = "find #{dir} -name \\\*.rb | #{CTAGS} #{CTAGS_FLAGS}"
+    cmd << " -f #{dir}/#{TAGS_FILE} -L - 2>>/dev/null"
+    cmd << ' &' if run_in_background
+    $stderr.print "[#{$PROGRAM_NAME}] calling #{cmd}\n" if DEBUG_MODE
     system cmd
   else
-    $stderr.print "[#{$0}] #{CTAGS} failed to write #{dir}/#{TAGS_FILE}\n"
+    $stderr.print(
+      "[#{$PROGRAM_NAME}] #{CTAGS} failed to write #{dir}/#{TAGS_FILE}\n")
   end
 end
 
 def install_hook(hook)
   if File.exists?(hook)
-    $stderr.print "A file already exists at #{hook}, and will NOT be replaced.\n"
+    $stderr.print(
+      "A file already exists at #{hook}, and will NOT be replaced.\n")
     return
   end
-  
+
   print "Linking #{__FILE__} to #{hook}\n"
-  %x{ln -s #{__FILE__}  #{hook}}
+  `ln -s #{__FILE__} #{hook}`
 end
 
 if ARGV.first == '-i'
